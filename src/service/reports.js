59943412838f586fb54a7f9e90b176bb859419fb
a-yerzhyk <andriiyerzh@gmail.com>
2023-09-08 19:05:57 +0300

    test: return reports

--- src/service/reports.js
+++ src/service/reports.js
@@ -1,130 +1,130 @@
-// import photo1 from '@/assets/images/reports/report1-1.jpg'
-// import photo2 from '@/assets/images/reports/report1-2.jpg'
-// import photo3 from '@/assets/images/reports/report1-3.jpg'
-// import photo4 from '@/assets/images/reports/report1-4.jpg'
-// import photo5 from '@/assets/images/reports/report1-5.jpg'
-// import photo6 from '@/assets/images/reports/report2-1.jpg'
-// import photo7 from '@/assets/images/reports/report2-2.jpg'
-// import photo8 from '@/assets/images/reports/report2-3.jpg'
-// import photo9 from '@/assets/images/reports/report2-4.jpg'
-// import photo10 from '@/assets/images/reports/report2-5.jpg'
-// import photo11 from '@/assets/images/reports/report2-6.jpg'
-// import photo12 from '@/assets/images/reports/report3-1.jpg'
-// import photo13 from '@/assets/images/reports/report3-2.jpg'
-// import photo14 from '@/assets/images/reports/report3-3.jpg'
-// import photo15 from '@/assets/images/reports/report3-4.jpg'
-// import photo16 from '@/assets/images/reports/report3-5.jpg'
-// import photo17 from '@/assets/images/reports/report3-6.jpg'
-// import photo18 from '@/assets/images/reports/report4-1.jpg'
-// import photo20 from '@/assets/images/reports/report4-2.jpg'
-// import photo21 from '@/assets/images/reports/report4-3.jpg'
-// import photo22 from '@/assets/images/reports/report4-4.jpg'
-// import photo23 from '@/assets/images/reports/report4-5.jpg'
-// import photo24 from '@/assets/images/reports/report4-6.jpg'
-// import photo25 from '@/assets/images/reports/report5-1.jpg'
-// import photo26 from '@/assets/images/reports/report5-2.jpg'
+import photo1 from '@/assets/images/reports/report1-1.jpg'
+import photo2 from '@/assets/images/reports/report1-2.jpg'
+import photo3 from '@/assets/images/reports/report1-3.jpg'
+import photo4 from '@/assets/images/reports/report1-4.jpg'
+import photo5 from '@/assets/images/reports/report1-5.jpg'
+import photo6 from '@/assets/images/reports/report2-1.jpg'
+import photo7 from '@/assets/images/reports/report2-2.jpg'
+import photo8 from '@/assets/images/reports/report2-3.jpg'
+import photo9 from '@/assets/images/reports/report2-4.jpg'
+import photo10 from '@/assets/images/reports/report2-5.jpg'
+import photo11 from '@/assets/images/reports/report2-6.jpg'
+import photo12 from '@/assets/images/reports/report3-1.jpg'
+import photo13 from '@/assets/images/reports/report3-2.jpg'
+import photo14 from '@/assets/images/reports/report3-3.jpg'
+import photo15 from '@/assets/images/reports/report3-4.jpg'
+import photo16 from '@/assets/images/reports/report3-5.jpg'
+import photo17 from '@/assets/images/reports/report3-6.jpg'
+import photo18 from '@/assets/images/reports/report4-1.jpg'
+import photo20 from '@/assets/images/reports/report4-2.jpg'
+import photo21 from '@/assets/images/reports/report4-3.jpg'
+import photo22 from '@/assets/images/reports/report4-4.jpg'
+import photo23 from '@/assets/images/reports/report4-5.jpg'
+import photo24 from '@/assets/images/reports/report4-6.jpg'
+import photo25 from '@/assets/images/reports/report5-1.jpg'
+import photo26 from '@/assets/images/reports/report5-2.jpg'
 
 const reports = [
-  // {
-  //   src: photo1,
-  //   alt: "photo1",
-  // },
-  // {
-  //   src: photo2,
-  //   alt: "photo2",
-  // },
-  // {
-  //   src: photo3,
-  //   alt: "photo3",
-  // },
-  // {
-  //   src: photo4,
-  //   alt: "photo4",
-  // },
-  // {
-  //   src: photo5,
-  //   alt: "photo5",
-  // },
-  // {
-  //   src: photo6,
-  //   alt: "photo6",
-  // },
-  // {
-  //   src: photo7,
-  //   alt: "photo7",
-  // },
-  // {
-  //   src: photo8,
-  //   alt: "photo8",
-  // },
-  // {
-  //   src: photo9,
-  //   alt: "photo9",
-  // },
-  // {
-  //   src: photo10,
-  //   alt: "photo10",
-  // },
-  // {
-  //   src: photo11,
-  //   alt: "photo11",
-  // },
-  // {
-  //   src: photo12,
-  //   alt: "photo12",
-  // },
-  // {
-  //   src: photo13,
-  //   alt: "photo13",
-  // },
-  // {
-  //   src: photo14,
-  //   alt: "photo14",
-  // },
-  // {
-  //   src: photo15,
-  //   alt: "photo15",
-  // },
-  // {
-  //   src: photo16,
-  //   alt: "photo16",
-  // },
-  // {
-  //   src: photo17,
-  //   alt: "photo17",
-  // },
-  // {
-  //   src: photo18,
-  //   alt: "photo18",
-  // },
-  // {
-  //   src: photo20,
-  //   alt: "photo20",
-  // },
-  // {
-  //   src: photo21,
-  //   alt: "photo21",
-  // },
-  // {
-  //   src: photo22,
-  //   alt: "photo22",
-  // },
-  // {
-  //   src: photo23,
-  //   alt: "photo23",
-  // },
-  // {
-  //   src: photo24,
-  //   alt: "photo24",
-  // },
-  // {
-  //   src: photo25,
-  //   alt: "photo25",
-  // },
-  // {
-  //   src: photo26,
-  //   alt: "photo26",
-  // },
+  {
+    src: photo1,
+    alt: "photo1",
+  },
+  {
+    src: photo2,
+    alt: "photo2",
+  },
+  {
+    src: photo3,
+    alt: "photo3",
+  },
+  {
+    src: photo4,
+    alt: "photo4",
+  },
+  {
+    src: photo5,
+    alt: "photo5",
+  },
+  {
+    src: photo6,
+    alt: "photo6",
+  },
+  {
+    src: photo7,
+    alt: "photo7",
+  },
+  {
+    src: photo8,
+    alt: "photo8",
+  },
+  {
+    src: photo9,
+    alt: "photo9",
+  },
+  {
+    src: photo10,
+    alt: "photo10",
+  },
+  {
+    src: photo11,
+    alt: "photo11",
+  },
+  {
+    src: photo12,
+    alt: "photo12",
+  },
+  {
+    src: photo13,
+    alt: "photo13",
+  },
+  {
+    src: photo14,
+    alt: "photo14",
+  },
+  {
+    src: photo15,
+    alt: "photo15",
+  },
+  {
+    src: photo16,
+    alt: "photo16",
+  },
+  {
+    src: photo17,
+    alt: "photo17",
+  },
+  {
+    src: photo18,
+    alt: "photo18",
+  },
+  {
+    src: photo20,
+    alt: "photo20",
+  },
+  {
+    src: photo21,
+    alt: "photo21",
+  },
+  {
+    src: photo22,
+    alt: "photo22",
+  },
+  {
+    src: photo23,
+    alt: "photo23",
+  },
+  {
+    src: photo24,
+    alt: "photo24",
+  },
+  {
+    src: photo25,
+    alt: "photo25",
+  },
+  {
+    src: photo26,
+    alt: "photo26",
+  },
 ]
 
 export default reports
