0b3869802150dcb913edfd4e9da3830e704c5092
a-yerzhyk <andriiyerzh@gmail.com>
2023-09-30 16:38:45 +0300

    feat: use toast on TextToCopy

---
+++ src/components/TextToCopy.jsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { useContext } from 'react'
+import { DictionaryContext } from '@/app/dictionaryProvider'
+import { useToast } from "@/hooks/useToast"
+
+import { classNames } from "@/utils/classNames"
+
+export default function TextToCopy ({ text, className }) {
+  const { dictionary } = useContext(DictionaryContext)
+  const { showToast } = useToast()
+
+  const copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text)
+    showToast(dictionary.copied_to_clipboard)
+  }
+
+  const handleClick = () => {
+    copyToClipboard(text)
+  }
+  return (
+    <span className={classNames(className, 'cursor-pointer')} onClick={handleClick}>
+      {text}
+    </span>
+  )
+}
